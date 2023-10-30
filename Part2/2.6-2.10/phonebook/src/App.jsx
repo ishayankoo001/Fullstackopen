1258cb0430ac47e5efe711eb5f1740270943fd4f
ishayankoo001 <ishayankoo@icloud.com>
2023-10-30 17:49:00 +0100

    Finished phonebook exercise

--- Part2/2.6-2.10/phonebook/src/App.jsx
+++ Part2/2.6-2.10/phonebook/src/App.jsx
@@ -1,34 +1,60 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Filter from './Filter'
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+  ])
 
-function App() {
-  const [count, setCount] = useState(0)
+  const handleInputChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setNewFilter(event.target.value)
+  }
+  const handleNumberInputChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+  const handleNameChange = (event)=>{
+    event.preventDefault()
+    console.log(newName)
+    if(persons.some(person => person.name === newName)){
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+    setPersons(persons.concat({name:newName, number:newNumber}))
+    setNewName('')
+    setNewNumber('')
+  }
 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+  const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={newFilter} handleFilterChange={handleFilterChange} />
+      <form>
+        <div>
+          name: <input value={newName}onChange={handleInputChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber}onChange={handleNumberInputChange}/>
+        </div>
+        <div>
+          <button type="submit" onClick={handleNameChange}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {filteredPersons.map((person) => (
+          <li key={person.name}>{person.name} {person.number}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
